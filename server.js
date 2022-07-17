f9e113e4256a795747503760975af4138051e979
Bilgihan Kose <bilgihankose@gmail.com>
2022-07-17 18:56:07 +0300

    Fix errorHandler middleware for getting JSON

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(errorHandler);
 app.use("/v1/api/users", require("./routes/userRoutes"));
 
 // Routes
 app.get("/v1/api/users", (req, res) => {
   res.status(200).json({ message: "Welcome to the Support Desk API" });
 });
+
+//For returning JSON error message
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
